914bb474b2aabcdcedd1d8220a32251a99ff00f2
Ssie8 <101572404+Ssie8@users.noreply.github.com>
2022-05-29 12:30:59 +0200

    update

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import easyocr
+import os
 from gtts import gTTS
+import easyocr
 from PIL import Image
 from numpy import asarray
 from googletrans import Translator
@@ -13,27 +14,26 @@ uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
 translator = Translator()
 
 if uploaded_file:
-  # display the file
-  display_image = Image.open(uploaded_file)
-  numpydata = asarray(display_image)
-  display_image = display_image.resize((500,300))
-  st.image(display_image)
-  pred_button = st.button('Read Image')
-  
-  if pred_button:
-    prediction = reader.readtext(numpydata, detail = 0)
-    prediction_2 = ' '.join(prediction)
-    st.text(f'Text inside image: {prediction_2}')
-    tts = gTTS(text=prediction_2)
-    tts.save('audio.mp3')
-    audio_file = open('audio.mp3', 'rb')
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes)
-    st.header('Translated to Italian')
-    it_result = translator.translate(prediction_2, dest='it')
-    st.text(it_result.text)
-    tts = gTTS(text=it_result.text, lang='it')
-    tts.save('it_audio.mp3')
-    audio_file = open('it_audio.mp3', 'rb')
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes)
+    # display the file
+    display_image = Image.open(uploaded_file)
+    numpydata = asarray(display_image)
+    display_image = display_image.resize((500,300))
+    st.image(display_image)
+    pred_button = st.button('Read Image')
+    
+    if pred_button:
+        prediction = ' '.join(reader.readtext(numpydata, detail = 0))
+        st.text(f'Predictions: {prediction}')
+        tts = gTTS(text=prediction)
+        tts.save('en_audio.mp3')
+        audio_file = open('en_audio.mp3', 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes)
+        st.header('Translated to Italian')
+        it_result = translator.translate(prediction, dest='it')
+        st.text(it_result.text)
+        tts_it = gTTS(text=it_result.text, lang='it')
+        tts_it.save('it_audio.mp3')
+        audio_file = open('it_audio.mp3', 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes)
